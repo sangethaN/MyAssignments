5b4283a0c0730d746f8b96f6cc741e6cf4465869
sangeetha N <71548091+sangethaN@users.noreply.github.com>
2023-04-01 23:54:08 +0530

    Update IsPrime.java

--- day1/IsPrime.java
+++ day1/IsPrime.java
@@ -3,15 +3,19 @@ package weeks.day1;
 public class IsPrime {
 
 	public static void main(String[] args) {
-		int n=31,flag=0;
+		int n=13,flag=0;
 		for(int i=2;i<n;i++) {
+
 			if(n%i==0 )
 			{ flag++;
 			System.out.println("Not Prime");
 			break;
 			}
 		}
-		if(flag==0)
+		if(n<=1) {
+			System.out.println("Neither Prime nor Composite");
+		}
+		else if(flag==0)
 			System.out.println("Prime");
 	}
 
